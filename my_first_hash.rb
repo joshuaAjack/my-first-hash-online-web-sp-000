e7b2121e44a0e0b20d07d147cc2e8d05f09de5fb
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-01 02:16:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
+def my_hash(hash)
+  hash = {"My_age" => "21"}
+  end
 
 
 def shipping_manifest
